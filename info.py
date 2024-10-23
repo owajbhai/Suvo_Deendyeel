eef75c1751be09410fc43da051ae0e35475669e4
sonudhakad778 <145945975+sonudhakad778@users.noreply.github.com>
2024-10-24 00:27:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001837694049') # request chan
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Spiderman:ohWHjNXG9VedoC5R@cluster0.uuauf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sonudhakad167:VnNcAKkuJRDkI0mP@cluster0.8jmli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
