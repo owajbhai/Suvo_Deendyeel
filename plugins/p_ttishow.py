0f4395927460848cb37519b33fe465f36ffa1076
saraswatjisupport <148682315+saraswatjisupport@users.noreply.github.com>
2024-12-14 15:40:31 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, OWNER_LNK, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK
-from database.users_chats_db import db
+from database.users_chats_db import db, db2
 from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
 from Script import script
@@ -158,12 +158,16 @@ async def get_ststs(bot, message):
     rju = await message.reply('ᴀᴄᴄᴇꜱꜱɪɴɢ ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ...')
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
-    files = await Media.count_documents()
     size = await db.get_db_size()
     free = 536870912 - size
     size = get_size(size)
     free = get_size(free)
-    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
+    files = await Media.count_documents()
+    size2 = await db2.get_db_size()
+    free2 = 536870912 - size2
+    size2 = get_size(size2)
+    free2 = get_size(free2)
+    await rju.edit(script.STATUS_TXT.format(total_users, totl_chats, size, free, files, size2, free2))
 
 
 @Client.on_message(filters.command('invite') & filters.user(ADMINS))
