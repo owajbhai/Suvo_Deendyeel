ad047c159ff45f25ea6b0a415f9d8b6df0c23b61
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-12-25 19:20:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -749,7 +749,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Sorry_Sorry_Galti_Ho_Gai")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Arpitbotmovies")
                                 ]
                             ]
                         )
