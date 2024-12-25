6a0243e8a5eacf7dd0c0430885b3691f03b46ad3
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-12-25 18:49:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -269,18 +269,11 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     CAPTION = """<b><a href='https://t.me/Arpitbotmovies'>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}</a>"""
 
     IMDB_TEMPLATE_TXT = """
-<b>Query: {qurey}
-
-IMDb Data:
 <b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
 📀 RunTime: {runtime} Minutes
-📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
-
 
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 
